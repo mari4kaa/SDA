ed2db301460a8ef57b0510104bbb80429ae37ac8
Mariia Kryvokhata <103357823+mari4kaa@users.noreply.github.com>
2023-03-17 00:08:51 +0200

    Sort the list

--- 2 sem lab2/lab2_2.c
+++ 2 sem lab2/lab2_2.c
@@ -1,7 +1,5 @@
-﻿#include <stdio.h>
-#define _CRTDBG_MAP_ALLOC
+#include <stdio.h>
 #include <stdlib.h>
-#include <crtdbg.h>
 
 typedef struct linked_list 
 {
@@ -14,23 +12,41 @@ l_list *l_list_init(char item);
 l_list *add_node(l_list *l_p, char item);
 l_list *del_node(l_list *l_p);
 char random_letter();
+l_list *sort(l_list *l_p);
 
 int main()
 {
-    _CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
-    _CrtSetReportMode(_CRT_WARN, _CRTDBG_MODE_DEBUG);
     unsigned int n;
     printf("Input number of items: ");
     scanf_s("%u", &n);
+    //create list
     char first_letter = random_letter();
     l_list *list_p = l_list_init(first_letter);
     l_list *first_node = list_p;
-    for (unsigned int i = 0; i < n; i++)
+    for (unsigned int i = 0; i < n - 1; i++)
     {
         char letter = random_letter();
         list_p = add_node(list_p, letter);
     }
-    //TODO: sort
+    list_p = first_node;
+    //print unsorted
+    printf("Unsorted list:\n");
+    l_list *iterator = first_node;
+    while(iterator != NULL)
+    {
+        printf("ptr=%d info =%c prev=%d next =%d\n", iterator, iterator->info, iterator->prev_node, iterator->next_node);
+        iterator = iterator->next_node;
+    }
+    //sort
+    list_p = sort(list_p);
+    //print sorted
+    printf("Sorted list:\n");
+    while (list_p != NULL)
+    {
+        printf("ptr=%d info =%c prev=%d next =%d\n", list_p, list_p->info, list_p->prev_node, list_p->next_node);
+        list_p = list_p->next_node;
+    }
+    //free
     while (list_p != NULL)
     {
         list_p = del_node(list_p);
@@ -46,26 +62,23 @@ l_list *l_list_init(char item)
             .prev_node = NULL,
             .next_node = NULL
            };
-    printf("ptr=%d info =%c prev=%d next =%d\n", l_p, l_p->info, l_p->prev_node, l_p->next_node);
     return l_p;
 }
 
 l_list *add_node(l_list *l_p, char item)
 {
-    struct linked_list *p_node;
+    l_list *p_node;
     p_node = malloc(sizeof(struct linked_list));
     p_node->info = item;
     p_node->prev_node = l_p;
     p_node->next_node = NULL;
     l_p->next_node = p_node;
-    printf("ptr=%d info =%c prev=%d next =%d\n", p_node, p_node->info, p_node->prev_node, p_node->next_node);
-    printf("ptr=%d info =%c prev=%d next =%d\n", l_p, l_p->info, l_p->prev_node, l_p->next_node);
     return p_node;
 }
 
 l_list *del_node(l_list *l_p)
 {
-    struct linked_list *p_node;
+    l_list *p_node;
     p_node = l_p->prev_node;
     if (l_p != NULL)
     {
@@ -80,4 +93,45 @@ char random_letter()
     int len = 'z' - 'a' + 1;
     letter = 'a' + (rand() % len);
     return letter;
+}
+
+l_list *sort(l_list *list_p)
+{
+    l_list *first_node = list_p;
+    l_list *current_node = first_node->next_node;
+    while (current_node != NULL)
+    {
+        char current_info = current_node->info;
+        l_list *current_next = current_node->next_node;
+        l_list *current_prev = current_node->prev_node;
+        l_list *p_node = first_node;
+        
+        while (p_node->info < current_info)
+        {
+            p_node = p_node->next_node;
+        }
+        if (p_node != current_node)
+        {
+            l_list *p_prev = p_node->prev_node;
+            if (p_prev != NULL)
+            {
+                p_prev->next_node = current_node;
+            }
+            else
+            {
+                first_node = current_node;
+            }
+            p_node->prev_node = current_node;
+            current_node->next_node = p_node;
+            current_node->prev_node = p_prev;
+            
+            if (current_next != NULL)
+            {
+                current_next->prev_node = current_prev;
+            }
+            current_prev->next_node = current_next;
+        }
+        current_node = current_next;
+    }
+    return first_node;
 }
